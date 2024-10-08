425f7c052cf4e3a2a6625bfad5bfe7f419da5616
Jongramdev <fas0001@fas0001ui-MacBookPro.local>
2024-10-08 16:19:31 +0900

    FEAT : [ 앱 스키마 www.fashionandstyle.com 변경 ]

--- bridge-page/src/bridge/BridgePage.js
+++ bridge-page/src/bridge/BridgePage.js
@@ -13,7 +13,8 @@ function BridgePage() {
           // Android 사용자일 경우 딥링크를 시도하고, 앱 스토어로 리다이렉트
           const iframe = document.createElement("iframe");
           iframe.style.display = "none";
-          iframe.src = "fashionandstyle://";
+        //   iframe.src = "fashionandstyle://";
+          iframe.src = "https://www.fashionandstyle.com";
           document.body.appendChild(iframe);
           setTimeout(() => {
             window.location = "https://play.google.com/store/apps/details?id=com.fas.android";
@@ -22,7 +23,8 @@ function BridgePage() {
           // iOS 사용자일 경우 딥링크를 시도하고, 앱 스토어로 리다이렉트
           const iframe = document.createElement("iframe");
           iframe.style.display = "none";
-          iframe.src = "fashionandstyle://";
+        //   iframe.src = "fashionandstyle://";
+          iframe.src = "https://www.fashionandstyle.com";
           document.body.appendChild(iframe);
           setTimeout(() => {
             window.location = "https://apps.apple.com/app/id1620312420";
@@ -38,7 +40,7 @@ function BridgePage() {
                 </h1>
                 <p>잠시 후 앱으로 이동합니다. 이동되지 않으면 앱 스토어에서 설치해주세요.</p>
                 <div>
-                    <a href="fashionandstyle://">앱에서 열기</a>
+                    <a href="https://www.fashionandstyle.com">앱에서 열기</a>
                     <p>또는</p>
                     <a href="https://apps.apple.com/app/id1620312420">앱스토어에서 설치</a>
                 </div>
