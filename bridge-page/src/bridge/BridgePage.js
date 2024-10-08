e6022b4d50e95a5094a5358f4005753c29880d40
Jongramdev <fas0001@fas0001ui-MacBookPro.local>
2024-10-08 15:46:01 +0900

    FEAT : [ 앱스토어 이동 자동 실행 수정 ]

--- bridge-page/src/bridge/BridgePage.js
+++ bridge-page/src/bridge/BridgePage.js
@@ -6,59 +6,55 @@ function BridgePage() {
         document.title = "패션&스타일";
     }, []);
 
-    // useEffect(() => {
-    //     const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-    
-    //     if (/android/i.test(userAgent)) {
-    //       // Android 사용자일 경우 딥링크를 시도하고, 앱 스토어로 리다이렉트
-    //       window.location = "fashionandstyle://";
-    //       setTimeout(() => {
-    //         window.location = "https://play.google.com/store/apps/details?id=com.fas.android";
-    //         setTimeout(() => {
-    //             window.close();
-    //         }, 500);
-    //       }, 2000);
-    //     } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-    //       // iOS 사용자일 경우 딥링크를 시도하고, 앱 스토어로 리다이렉트
-    //       window.location = "fashionandstyle://";
-    //       setTimeout(() => {
-    //         window.location = "https://apps.apple.com/app/id1620312420";
-    //         setTimeout(() => {
-    //             window.close();
-    //         }, 500);
-    //       }, 2000);
-    //     }
-    //   }, []);
-
-    const handleLinkClick = () => {
+    useEffect(() => {
         const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-
+    
         if (/android/i.test(userAgent)) {
-            // Android 사용자일 경우 딥링크를 시도하고, 앱 스토어로 리다이렉트
-            window.location = "fashionandstyle://";
+          // Android 사용자일 경우 딥링크를 시도하고, 앱 스토어로 리다이렉트
+          window.location = "fashionandstyle://";
+          setTimeout(() => {
+            window.location = "https://play.google.com/store/apps/details?id=com.fas.android";
             setTimeout(() => {
-                window.location = "https://play.google.com/store/apps/details?id=com.fas.android";
-            }, 2000);
+                window.close();
+            }, 500);
+          }, 2000);
         } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-            // iOS 사용자일 경우 딥링크를 시도하고, 앱 스토어로 리다이렉트
-            window.location = "fashionandstyle://";
+          // iOS 사용자일 경우 딥링크를 시도하고, 앱 스토어로 리다이렉트
+          window.location = "fashionandstyle://";
+          setTimeout(() => {
+            window.location = "https://apps.apple.com/app/id1620312420";
             setTimeout(() => {
-                window.location = "https://apps.apple.com/app/id1620312420";
-            }, 2000);
+                window.close();
+            }, 500);
+          }, 2000);
         }
-    };
+      }, []);
+
+    // const handleLinkClick = () => {
+    //     const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+    //     if (/android/i.test(userAgent)) {
+    //         // Android 사용자일 경우 딥링크를 시도하고, 앱 스토어로 리다이렉트
+    //         window.location = "fashionandstyle://";
+    //         setTimeout(() => {
+    //             window.location = "https://play.google.com/store/apps/details?id=com.fas.android";
+    //         }, 2000);
+    //     } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    //         // iOS 사용자일 경우 딥링크를 시도하고, 앱 스토어로 리다이렉트
+    //         window.location = "fashionandstyle://";
+    //         setTimeout(() => {
+    //             window.location = "https://apps.apple.com/app/id1620312420";
+    //         }, 2000);
+    //     }
+    // };
 
     return(
         <>
             <div>
                 <h1>
-                    브릿지 페이지 입니디.
+                    브릿지 페이지 입니다.
                 </h1>
                 <p>잠시 후 앱으로 이동합니다. 이동되지 않으면 앱 스토어에서 설치해주세요.</p>
-   
-                <button onClick={handleLinkClick}>
-                    앱 스토어로 이동하기
-                </button>
             </div>
         </>
     );
