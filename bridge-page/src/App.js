d086bf7365f39d539d0d9eec8b2dddd658e7c124
Jongramdev <fas0001@fas0001ui-MacBookPro.local>
2024-10-10 18:45:11 +0900

    FEAT : [ react-router-dom 추가 ]

--- bridge-page/src/App.js
+++ bridge-page/src/App.js
@@ -1,12 +1,22 @@
 // import logo from './logo.svg';
+// import './App.css';
+// import BridgePage from './bridge/BridgePage';
+import React from 'react';
 import './App.css';
 import BridgePage from './bridge/BridgePage';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <BridgePage/>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/bridge" element={<BridgePage />} />
+          <Route path="/" element={<BridgePage />} /> {/* 홈 경로 */}
+          {/* 필요한 다른 라우트 추가 */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
